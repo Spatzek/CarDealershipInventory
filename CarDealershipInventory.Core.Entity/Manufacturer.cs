3683ea47edc76484499eb75d3edd6c1e5ebc121f
RainbowMellow <christina.nygaard@outlook.com>
2020-11-27 11:04:40 +0100

    Added entities

---
+++ CarDealershipInventory.Core.Entity/Manufacturer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealershipInventory.Core.Entity
+{
+    public class Manufacturer
+    {
+        public int ManufacturerId { get; set; }
+        public string Name { get; set; }
+        public List<Model> Models { get; set; }
+    }
+}
---
+++ CarDealershipInventory.Core.Entity/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealershipInventory.Core.Entity
+{
+    public class Model
+    {
+        public int ModelId { get; set; }
+        public string Name { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public int ManufacturerId { get; set; }
+
+    }
+}
